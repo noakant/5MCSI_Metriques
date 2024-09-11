368853997471208d3dc48de648adf2a856e92a31
noakant <91458205+noakant@users.noreply.github.com>
2024-09-11 10:03:48 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -41,6 +41,9 @@ def extract_minutes(date_string):
         minutes = date_object.minute
         return jsonify({'minutes': minutes})
 
+@app.route("/commits/")
+def moncommits():
+    return render_template("commits.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
